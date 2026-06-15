cf0c487ce8a097a34d296d39b8e9c383f00df8fa
RajendraAmbhore <ambhorerajendra214@gmail.com>
2026-06-15 23:37:25 +0530

    Add files via upload

---
+++ Assignment 2/Arithmaticoperation.py
@@ -0,0 +1,16 @@
+print("Enter first number :")
+No1 = input()
+
+print("Enter first number :")
+No2 = input()
+
+Ans1 = int(No1) + int(No2)
+Ans2 = int(No1) - int(No2)
+Ans3 = int(No1) * int(No2)
+Ans4 = int(No1) / int(No2)
+
+
+print("Addition is:",Ans1)
+print("Subtraction is:",Ans2)
+print("Multiplication is:",Ans3)
+print("Division is:",Ans4)
---
+++ Assignment 2/Display.py
@@ -0,0 +1,8 @@
+print("Enter your name :")
+name = input()
+
+print("Enter your age :")
+age = int(input())
+next_age = age + 1
+
+print("Hello {name} you will turn {next_age} next year.")
