4a66443f64ce581128d744e646f14ed2c975240c
RajendraAmbhore <ambhorerajendra214@gmail.com>
2026-06-15 23:42:53 +0530

    Delete Builtinfunction.py

--- Builtinfunction.py
+++
@@ -1,6 +0,0 @@
-no = 15                 
-
-print("Value of no is:",no)               #value 
-print("type of no is :",type(no))         #type
-print("id of no is :",id(no))             #memory address
-
