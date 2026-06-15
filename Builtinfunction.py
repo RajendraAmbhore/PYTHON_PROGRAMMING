3d49fb190d3d8c95f548a39720f5cef0bf6dfa05
RajendraAmbhore <ambhorerajendra214@gmail.com>
2026-06-15 23:02:48 +0530

    Add files via upload

---
+++ Builtinfunction.py
@@ -0,0 +1,6 @@
+no = 15                 
+
+print("Value of no is:",no)               #value 
+print("type of no is :",type(no))         #type
+print("id of no is :",id(no))             #memory address
+
